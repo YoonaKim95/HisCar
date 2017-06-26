3f02d56a322a536c4bdc3b972aca4078ad86c24d
NamsooC <thinkcnns@gmail.com>
2017-06-26 10:49:04 +0900

    no message

--- 20170622/hi.cpp
+++ 20170622/hi.cpp
@@ -19,15 +19,17 @@ float slopeR_Pre = 0;
 float slope_Pre = 0;
 
 
+
 void lane_detection(Mat frame)
 {
     Mat gray, smot, sub, hsv, white, yellow, both, canny, left, right;
     
     Mat sub1, sub2, sub3, sub4, smot1, smot2, smot3, smot4, gray1, gray2, gray3, gray4, white1, white2, white3, white4;
-    Mat canny1, canny2, canny3, canny4, left1, left2, left3, left4, right1, right2, right3, right4;
+    Mat canny1, canny2, canny3, canny4, left1, left2, left3, left4, right1, right2, right3, right4,hlsImg;
     
+    vector<Mat> channels(hlsImg.channels()); // vector Mat for histogram
     vector<Vec4i> lines;
-    
+    //string A[100] = {gray1,sub1,smot1};
     // throwing away the first frame
     
     
@@ -62,6 +64,8 @@ void lane_detection(Mat frame)
     
     //Point of center.중점은 검은색으로 고정시켰음.
      circle(frame, Point(frame.cols/2,frame.rows/4), 5, Scalar(0,0,0), 3, LINE_AA);
+    
+    
     /*
      cout << "col: " << frame.cols << endl;
      cout << "hei: " << frame.rows << endl;
@@ -108,6 +112,9 @@ void lane_detection(Mat frame)
     // bgr2gray
     cvtColor(sub, gray, CV_BGR2GRAY);
     
+    
+    
+    //parts
     cvtColor(sub1, gray1, CV_BGR2GRAY);
     cvtColor(sub2, gray2, CV_BGR2GRAY);
     cvtColor(sub3, gray3, CV_BGR2GRAY);
@@ -130,7 +137,12 @@ void lane_detection(Mat frame)
     left = canny(left_rec);
     right = canny(right_rec);
     
-    /*************edge  detection  1 **************/
+    
+    
+    
+    
+    
+    /*************edge  detection  3 **************/
     Canny(white3, canny3, 150, 300, 3);
     imshow("canny3", canny3);
     left3 = canny(left_rec3);
@@ -219,101 +231,20 @@ void lane_detection(Mat frame)
     //   line(frame, Point(x1 / countright + x, y1 / countright + y), Point(x2 / countright + x, y2 / countright + y), Scalar(0, 255, 0), 2);
     //   line(frame, Point(x3 / countleft + x, y3 / countleft + y), Point(x4 / countleft + x, y4 / countleft + y), Scalar(0, 255, 0), 2);
     circle(frame, Point(X3.at<float>(0, 0), X3.at<float>(1, 0)), 5, LaneColor3, 3, LINE_AA);
+//    
+//    if(X3.at<float>(0, 0)>frame.cols/2)
+//    {
+//        putText(frame, "Turn right" , Point(300,400), FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor3), 2, LINE_8);
+//    }
+//    else
+//    {
+//        putText(frame, "Turn left" ,Point(300,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor3), 2, LINE_8);
+//    }
+
     
     /*************edge  detection  2 **************/
     
-    
-    /*************edge  detection  1 **************/
-    Canny(white1, canny1, 150, 300, 3);
-    imshow("canny1", canny1);
-    left1 = canny(left_rec1);
-    right1 = canny(right_rec1);
-    
-    vector<Vec4i> lines_R1;
-    vector<Point> pointList_R1;
-    
-    Point vp_R1;
-    float c1_R1, c2_R1;
-    
-    
-    //20, 10, 140
-    HoughLinesP(canny1, lines_R1, 1, CV_PI / 180, 20, 10, 140);
-    //Merge part
-    
-    for (size_t i = 0; i < lines_R1.size(); i++) {
-        Vec4i l = lines_R1[i];
-        
-        float slope = ((float)l[1] - (float)l[1]) / ((float)l[2] - (float)l[0]);
-        
-        if (slope >= 0.1 && slope <= 1) {
-            //           line(frame, Point(l[0], l[1] + rec1_point.y), Point(l[2], l[1] + rec1_point.y), LaneColor1, 2);
-            countright++;
-            x1 += l[0];
-            y1 += l[1] + rec1_point.y;
-            x2 += l[2];
-            y2 += l[3] + rec1_point.y;
-        }
-        if (slope <= -0.3 && slope >= -3) {
-            //       line(frame, Point(l[0], l[1] + rec3_point.y), Point(l[2], l[3]+ rec3_point.y), LaneColor3, 2);
-            countleft++;
-            x3 += l[0];
-            y3 += l[1] + rec3_point.y;
-            x4 += l[2];
-            y4 += l[3] + rec3_point.y;
-        }
-    }
-    
-    if (countright == 0) {
-        x1 = xp1;
-        x2 = xp2;
-        y1 = yp1;
-        y2 = yp2;
-    }
-    //º±¿Ã æ»¿‚»˜∏È ¿Ã¿¸ «¡∑π¿”¿ª æ≤¥¬∞Õ.
-    if (countleft == 0) {
-        x3 = xp3; //xp¥¬ past x.
-        x4 = xp4;
-        y3 = yp3;
-        y4 = yp4;
-    }
-    
-    Rslope = (y2 - y1) / (x2 - x1);
-    Lslope = (y4 - y3) / (x4 - x3);
-    
-    rb = (y1 / countright + y) - Rslope * (x1 / countright + x);
-    lb = (y3 / countleft + y) - Lslope * (x3 / countleft + x);
-    
-    lastx1 = (0 - rb) / Rslope;
-    lastx2 = (frame.rows - rb) / Rslope;
-    
-    lastx3 = ((0 - lb) / Lslope);
-    lastx4 = ((frame.rows - lb) / Lslope);
-    
-    //º“Ω«¡° ±∏«œ¥¬ ∆ƒ∆Æ
-    
-    a1 = lastx1 + x, a2 = lastx2 + x, a3 = lastx3 + x, a4 = lastx4 + x;
-    b1 = 0, b2 = frame.rows, b3 = 0, b4 = frame.rows;
-    
-    float dataA2[] = { (b2 - b1) / (a2 - a1), -1, (b4 - b3) / (a4 - a3), -1 };
-    Mat A2(2, 2, CV_32F, dataA2);
-    Mat invA2;
-    invert(A2, invA2);
-    
-    float dataB2[] = { a1*(b2 - b1) / (a2 - a1) - b1, a3*(b4 - b3) / (a4 - a3) - b3 };
-    Mat B2(2, 1, CV_32F, dataB2);
-    
-    Mat X2 = invA3*B3;
-    
-    //   cout << X << endl;
-    //      cout << X.at<float>(0, 0) << endl;
-    
-    line(frame, Point(a1, 0), Point(a2, frame.rows), LaneColor3, 3);
-    line(frame, Point(a3, 0), Point(a4, frame.rows), LaneColor3, 3);
-    //   line(frame, Point(x1 / countright + x, y1 / countright + y), Point(x2 / countright + x, y2 / countright + y), Scalar(0, 255, 0), 2);
-    //   line(frame, Point(x3 / countleft + x, y3 / countleft + y), Point(x4 / countleft + x, y4 / countleft + y), Scalar(0, 255, 0), 2);
-    circle(frame, Point(X2.at<float>(0, 0), X2.at<float>(1, 0)), 5, LaneColor3, 3, LINE_AA);
-    
-    /*************edge  detection  4 **************/
+      /*************edge  detection  4 **************/
     
     
     Canny(white4, canny4, 150, 300, 3);
@@ -405,7 +336,39 @@ void lane_detection(Mat frame)
     
     
     circle(frame, Point(X4.at<float>(0, 0), X4.at<float>(1, 0)), 5, LaneColor4, 3, LINE_AA);
-   }
+//    if(X4.at<float>(0, 0)>frame.cols/2)
+//    {
+//    putText(frame, "Turn right" , Point(100,400), FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+//    }
+//    else
+//    {
+//        putText(frame, "Turn left" ,Point(100,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+//    }
+    
+    if(((X4.at<float>(0, 0)>frame.cols/2)&&(X3.at<float>(0, 0)>frame.cols/2)))
+       {
+           putText(frame, "Turn right" ,Point(100,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+       }
+    else if(((X4.at<float>(0, 0)<frame.cols/2)&&(X3.at<float>(0, 0)<frame.cols/2)))
+        {
+            putText(frame, "Turn left" ,Point(100,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+        }
+       
+    
+    //histogram equalization.
+    cvtColor(frame, hlsImg, COLOR_BGR2HLS_FULL);
+
+    split(hlsImg,channels);
+    equalizeHist(channels[1], channels[1]);
+    merge(channels, hlsImg);
+    cvtColor(hlsImg, frame, COLOR_HLS2BGR_FULL);
+    imshow("hlsImg", frame);
+    
+    //histogram graph.
+      }
+
+
+
 
 int main() {
     char title[100] = "/Users/NAMSOO/Documents/Xcode/OpenCV/SelfDriving/SelfDriving/mono.mp4";
