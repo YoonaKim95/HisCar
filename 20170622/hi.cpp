7dfd69f1000d3c7b1ab16fd47d183e49c178fe65
NamsooC <thinkcnns@gmail.com>
2017-07-13 11:10:21 +0900

    MSAC with marking contour

--- 20170622/hi.cpp
+++ 20170622/hi.cpp
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+//#include "opencv2\highgui\highgui.hpp"
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
 #include "opencv2/opencv.hpp"
 #include <cmath>
 #include "opencv/cv.hpp"
@@ -13,6 +17,7 @@ using namespace cv;
 #define LaneColor3 Scalar(251,10,255)
 #define LaneColor4 Scalar(255,200,20)
 
+<<<<<<< HEAD
 float slopeL_PRE3 = 0 ;
 float slopeR_PRE3 = 0;
 float plb = 0;
@@ -29,10 +34,25 @@ void lane_detection(Mat frame)
     Mat sub1, sub2, sub3, sub4, smot1, smot2, smot3, smot4, gray1, gray2, gray3, gray4, white1, white2, white3, white4,gray_original_sub;
     Mat canny1, canny2, canny3, canny4, left1, left2, left3, left4, right1, right2, right3, right4,hlsImg,hist;
     Mat grayl3,grayl4,grayr3,grayr4; //gray areas.
+=======
+
+float slopeR_Pre = 0;
+float slope_Pre = 0;
+
+
+
+void lane_detection(Mat frame)
+{
+    Mat gray, smot, sub, hsv, white, yellow, both, canny, left, right;
+    
+    Mat sub1, sub2, sub3, sub4, smot1, smot2, smot3, smot4, gray1, gray2, gray3, gray4, white1, white2, white3, white4;
+    Mat canny1, canny2, canny3, canny4, left1, left2, left3, left4, right1, right2, right3, right4,hlsImg;
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     vector<Mat> channels(hlsImg.channels()); // vector Mat for histogram
     vector<Vec4i> lines;
     //string A[100] = {gray1,sub1,smot1};
+<<<<<<< HEAD
     
     // Variable for white detection
     int inRangeMaxl3 = 0,inRangeMaxl4 = 0,inRangeMaxr3 = 0,inRangeMaxr4 = 0;
@@ -43,10 +63,24 @@ void lane_detection(Mat frame)
     float x3 = 0, x4 = 0, y3 = 0, y4 = 0;
     
     //initiate for the first time when there are no input values.
+=======
+    // throwing away the first frame
+    
+    
+    float x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+    float x3 = 0, x4 = 0, y3 = 0, y4 = 0;
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     float countright = 0, countleft = 0;
     float xp1 = 0, xp2 = 0, yp1 = 0, yp2 = 0;
     float xp3 = 0, xp4 = 0, yp3 = 0, yp4 = 0;
     
+<<<<<<< HEAD
+=======
+    bool stop(false);
+    int key = 0;
+    int delay = 20;
+    int MAX_KERNEL_LENGTH = 7;
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     float a1 = 0, a2 = 0, a3 = 0, a4 = 0;
     float b1 = 0, b2 = 0, b3 = 0, b4 = 0;
     
@@ -57,6 +91,7 @@ void lane_detection(Mat frame)
     
     // resolution: 640 * 480
     //// mono.wmv :
+<<<<<<< HEAD
     int interest_y = 168; //ROI value of y
     int interest_x = 0;
     int width = frame.cols - interest_x; //width of ROI
@@ -102,22 +137,84 @@ void lane_detection(Mat frame)
     Rect right_rec4(rec4_Rpoint, Size(halfWidth , subROIHeight * 10));
     
     sub = frame(rec);
+=======
+    int interest_y = 168;
+    int interest_x = 0;
+    int width = frame.cols - interest_x - 00;
+    int height = frame.rows - interest_y - 0;
+    float leftmin = -0.5;
+    float rightmin = 0.6;
+    
+    int subROIHeight = height / 16;  // Calculate the height of sub_ROIs
+    
+    //Point of center.중점은 검은색으로 고정시켰음.
+     circle(frame, Point(frame.cols/2,frame.rows/4), 5, Scalar(0,0,0), 3, LINE_AA);
+    
+    
+    /*
+     cout << "col: " << frame.cols << endl;
+     cout << "hei: " << frame.rows << endl;
+     cout << "roi: " << subROIHeight << endl;
+     */
+    //height = 312 / 3 = 10
+    
+    Rect rec(interest_x, interest_y, width, height);  // overall ROI
+    
+    Rect rec1(interest_x, interest_y, width, subROIHeight * 2); // ROI1
+    Point rec1_point(interest_x, interest_y);
+    
+    Rect rec2(interest_x, interest_y + subROIHeight, width, subROIHeight * 2); // ROI 2
+    Point rec2_point(interest_x, interest_y + subROIHeight);
+    
+    Rect rec3(interest_x, interest_y + subROIHeight * 3, width, subROIHeight * 5); // ROI3
+    Point rec3_point(interest_x, interest_y + subROIHeight * 3);
+    
+    Rect rec4(interest_x, interest_y + subROIHeight * 6, width, subROIHeight * 10);
+    Point rec4_point(interest_x, interest_y + subROIHeight * 6);
+    
+    Rect left_rec(0, 0, width / 2, height);
+    
+    Rect left_rec1(0, 0, width / 2, subROIHeight * 2);
+    Rect left_rec2(0, subROIHeight, width / 2, subROIHeight * 2);
+    Rect left_rec3(0, subROIHeight * 3, width / 2, subROIHeight * 5);
+    Rect left_rec4(0, subROIHeight * 6, width / 2, subROIHeight * 10);
+    
+    
+    Rect right_rec(width / 2, 0, width / 2, height);
+    
+    Rect right_rec1(width / 2, 0, width / 2, subROIHeight * 2);
+    Rect right_rec2(width / 2, subROIHeight, width / 2, subROIHeight * 2);
+    Rect right_rec3(width / 2, subROIHeight * 3, width / 2, subROIHeight * 5);
+    Rect right_rec4(width / 2, subROIHeight * 6, width / 2, subROIHeight * 10);
+    
+    sub = frame(rec);
+    GaussianBlur(sub, sub, Size(3, 3), 0, 0);
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     sub1 = frame(rec1);
     sub2 = frame(rec2);
     sub3 = frame(rec3);
     sub4 = frame(rec4);
     
+<<<<<<< HEAD
     //show Left and Right ROI
     
     // bgr2gray
     cvtColor(sub, gray, CV_BGR2GRAY);
     
+=======
+    // bgr2gray
+    cvtColor(sub, gray, CV_BGR2GRAY);
+    
+    
+    
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     //parts
     cvtColor(sub1, gray1, CV_BGR2GRAY);
     cvtColor(sub2, gray2, CV_BGR2GRAY);
     cvtColor(sub3, gray3, CV_BGR2GRAY);
     cvtColor(sub4, gray4, CV_BGR2GRAY);
     
+<<<<<<< HEAD
     //apply gray parts to frame_gray.
     
     grayl3 = frame_gray(left_rec3);
@@ -179,17 +276,50 @@ void lane_detection(Mat frame)
     imshow("gray_frame",frame_gray);
     imshow("grayr4",grayr4);
     imshow("grayr3",grayr3);
+=======
+    /*************white detection**************/
+    
+    inRange(gray, 180, 255, white);
+    //ªÛºˆ∞° æ∆¥œ∂Û øµªÛ¿« π‡±‚∏¶ ∫∏∞Ì adaptive«œ∞‘ ∏∏µÈæÓæﬂ «—¥Ÿ.
+    //øµªÛ¿« π‡±‚∏¶ ∏Ó«¡∑π¿” ±‚¡ÿ¿∏∑Œ min, «•¡ÿ∆Ì¬˜, ∆Ú±’¿ª ∫∏∞Ì ¡§«ÿæﬂ «—¥Ÿ.
+    //¬˜º±¿∫ min∞™ ∫∏¥Ÿ¥¬ ¥ı π‡æ∆æﬂ «—¥Ÿ. æÛ∏∂≥™ ¥ı π‡æ∆æﬂ «œ¥¬∞°? sigma »∞øÎ. ≈Î∞Ë ∞™ »∞øÎ.
+    inRange(gray1, 180, 255, white1);
+    inRange(gray2, 180, 255, white2);
+    inRange(gray3, 180, 255, white3);
+    inRange(gray4, 180, 255, white4);
+    
+    
+    /*************edge detection**************/
+    Canny(white, canny, 150, 300, 3);
+    left = canny(left_rec);
+    right = canny(right_rec);
+    
+    
+    
+    
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     
     /*************edge  detection  3 **************/
     Canny(white3, canny3, 150, 300, 3);
     imshow("canny3", canny3);
+<<<<<<< HEAD
+=======
+    left3 = canny(left_rec3);
+    right3 = canny(right_rec3);
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     vector<Vec4i> lines_R3;
     vector<Point> pointList_R3;
     
+<<<<<<< HEAD
     //vanishing point VP
     Point vp_R3;
+=======
+    Point vp_R3;
+    float c1_R3, c2_R3;
+    
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     //20, 10, 140
     HoughLinesP(canny3, lines_R3, 1, CV_PI / 180, 20, 10, 140);
@@ -200,16 +330,26 @@ void lane_detection(Mat frame)
         
         float slope = ((float)l[3] - (float)l[1]) / ((float)l[2] - (float)l[0]);
         
+<<<<<<< HEAD
         //lines of left side
         if (slope >= 0.3 && slope <= 3) {
+=======
+        if (slope >= 0.3 && slope <= 3) {
+            //           line(frame, Point(l[0], l[1] + rec3_point.y), Point(l[2], l[3] + rec3_point.y), LaneColor3, 2);
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
             countright++;
             x1 += l[0];
             y1 += l[1] + rec3_point.y;
             x2 += l[2];
             y2 += l[3] + rec3_point.y;
         }
+<<<<<<< HEAD
         //lines of right side
         if (slope <= -0.3 && slope >= -3) {
+=======
+        if (slope <= -0.3 && slope >= -3) {
+            //       line(frame, Point(l[0], l[1] + rec3_point.y), Point(l[2], l[3]+ rec3_point.y), LaneColor3, 2);
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
             countleft++;
             x3 += l[0];
             y3 += l[1] + rec3_point.y;
@@ -217,13 +357,21 @@ void lane_detection(Mat frame)
             y4 += l[3] + rec3_point.y;
         }
     }
+<<<<<<< HEAD
     //if it is the first time, put initial values.
+=======
+    
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     if (countright == 0) {
         x1 = xp1;
         x2 = xp2;
         y1 = yp1;
         y2 = yp2;
     }
+<<<<<<< HEAD
+=======
+    //º±¿Ã æ»¿‚»˜∏È ¿Ã¿¸ «¡∑π¿”¿ª æ≤¥¬∞Õ.
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     if (countleft == 0) {
         x3 = xp3; //xp¥¬ past x.
         x4 = xp4;
@@ -231,6 +379,7 @@ void lane_detection(Mat frame)
         y4 = yp4;
     }
     
+<<<<<<< HEAD
     //slopes of right and left.
     float Rslope = (y2 - y1) / (x2 - x1);
     float Lslope = (y4 - y3) / (x4 - x3);
@@ -292,6 +441,24 @@ void lane_detection(Mat frame)
         line(frame, Point(a3, 0), Point(a4, frame.rows), LaneColor3, 3);
     }
     
+=======
+    float Rslope = (y2 - y1) / (x2 - x1);
+    float Lslope = (y4 - y3) / (x4 - x3);
+    
+    float rb = (y1 / countright + y) - Rslope * (x1 / countright + x);
+    float lb = (y3 / countleft + y) - Lslope * (x3 / countleft + x);
+    
+    float lastx1 = (0 - rb) / Rslope;
+    float lastx2 = (frame.rows - rb) / Rslope;
+    
+    float lastx3 = ((0 - lb) / Lslope);
+    float lastx4 = ((frame.rows - lb) / Lslope);
+    
+    //º“Ω«¡° ±∏«œ¥¬ ∆ƒ∆Æ
+    
+    a1 = lastx1 + x, a2 = lastx2 + x, a3 = lastx3 + x, a4 = lastx4 + x;
+    b1 = 0, b2 = frame.rows, b3 = 0, b4 = frame.rows;
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     float dataA[] = { (b2 - b1) / (a2 - a1), -1, (b4 - b3) / (a4 - a3), -1 };
     Mat A3(2, 2, CV_32F, dataA);
@@ -301,6 +468,7 @@ void lane_detection(Mat frame)
     float dataB[] = { a1*(b2 - b1) / (a2 - a1) - b1, a3*(b4 - b3) / (a4 - a3) - b3 };
     Mat B3(2, 1, CV_32F, dataB);
     
+<<<<<<< HEAD
     //vanishing point.
     Mat X3 = invA3*B3;
     
@@ -309,12 +477,44 @@ void lane_detection(Mat frame)
     
     
     /*************edge  detection  4 **************/
+=======
+    Mat X3 = invA3*B3;
+    
+    //   cout << X << endl;
+    //      cout << X.at<float>(0, 0) << endl;
+    
+    line(frame, Point(a1, 0), Point(a2, frame.rows), LaneColor3, 3);
+    line(frame, Point(a3, 0), Point(a4, frame.rows), LaneColor3, 3);
+    //   line(frame, Point(x1 / countright + x, y1 / countright + y), Point(x2 / countright + x, y2 / countright + y), Scalar(0, 255, 0), 2);
+    //   line(frame, Point(x3 / countleft + x, y3 / countleft + y), Point(x4 / countleft + x, y4 / countleft + y), Scalar(0, 255, 0), 2);
+    circle(frame, Point(X3.at<float>(0, 0), X3.at<float>(1, 0)), 5, LaneColor3, 3, LINE_AA);
+//    
+//    if(X3.at<float>(0, 0)>frame.cols/2)
+//    {
+//        putText(frame, "Turn right" , Point(300,400), FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor3), 2, LINE_8);
+//    }
+//    else
+//    {
+//        putText(frame, "Turn left" ,Point(300,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor3), 2, LINE_8);
+//    }
+
+    
+    /*************edge  detection  2 **************/
+    
+      /*************edge  detection  4 **************/
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     
     Canny(white4, canny4, 150, 300, 3);
     imshow("canny4", canny4);
+<<<<<<< HEAD
     
     dilate(white4,white4,Mat(),Point(-1,-1),5);
+=======
+    left4 = canny(left_rec4);
+    right4 = canny(right_rec4);
+    
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     vector<Vec4i> lines_R4;
     vector<Point> pointList_R4;
     
@@ -348,6 +548,7 @@ void lane_detection(Mat frame)
             y4 += l[3] + rec4_point.y;
         }
     }
+<<<<<<< HEAD
     
     /*
      Rslope = (y2 - y1) / (x2 - x1);
@@ -427,6 +628,40 @@ void lane_detection(Mat frame)
         line(frame, Point(a3, 0), Point(a4, frame.rows), LaneColor4, 3);
     }
     
+=======
+    /*
+     if (countright == 0) {
+     x1 = xp1;
+     x2 = xp2;
+     y1 = yp1;
+     y2 = yp2;
+     }
+     //º±¿Ã æ»¿‚»˜∏È ¿Ã¿¸ «¡∑π¿”¿ª æ≤¥¬∞Õ.
+     if (countleft == 0) {
+     x3 = xp3; //xp¥¬ past x.
+     x4 = xp4;
+     y3 = yp3;
+     y4 = yp4;
+     }
+     */
+    
+    Rslope = (y2 - y1) / (x2 - x1);
+    Lslope = (y4 - y3) / (x4 - x3);
+    
+    rb = (y1 / countright + y) - Rslope * (x1 / countright + x);
+    lb = (y3 / countleft + y) - Lslope * (x3 / countleft + x);
+    
+    lastx1 = (0 - rb) / Rslope;
+    lastx2 = (frame.rows - rb) / Rslope;
+    
+    lastx3 = ((0 - lb) / Lslope);
+    lastx4 = ((frame.rows - lb) / Lslope);
+    
+    //º“Ω«¡° ±∏«œ¥¬ ∆ƒ∆Æ
+    
+    a1 = lastx1 + x, a2 = lastx2 + x, a3 = lastx3 + x, a4 = lastx4 + x;
+    b1 = 0, b2 = frame.rows, b3 = 0, b4 = frame.rows;
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     float dataA4[] = { (b2 - b1) / (a2 - a1), -1, (b4 - b3) / (a4 - a3), -1 };
     Mat A4(2, 2, CV_32F, dataA4);
@@ -438,6 +673,7 @@ void lane_detection(Mat frame)
     
     Mat X4 = invA4*B4;
     
+<<<<<<< HEAD
     //  line(frame, Point(a1, 0), Point(a2, frame.rows), LaneColor4, 3);
     //  line(frame, Point(a3, 0), Point(a4, frame.rows), LaneColor4, 3);
     
@@ -465,6 +701,54 @@ int main() {
     VideoCapture capture(title);
     Mat frame;
     Mat origin;
+=======
+    //   cout << X << endl;
+    //      cout << X.at<float>(0, 0) << endl;
+    
+    line(frame, Point(a1, 0), Point(a2, frame.rows), LaneColor4, 3);
+    line(frame, Point(a3, 0), Point(a4, frame.rows), LaneColor4, 3);
+    
+    
+    circle(frame, Point(X4.at<float>(0, 0), X4.at<float>(1, 0)), 5, LaneColor4, 3, LINE_AA);
+//    if(X4.at<float>(0, 0)>frame.cols/2)
+//    {
+//    putText(frame, "Turn right" , Point(100,400), FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+//    }
+//    else
+//    {
+//        putText(frame, "Turn left" ,Point(100,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+//    }
+    
+    if(((X4.at<float>(0, 0)>frame.cols/2)&&(X3.at<float>(0, 0)>frame.cols/2)))
+       {
+           putText(frame, "Turn right" ,Point(100,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+       }
+    else if(((X4.at<float>(0, 0)<frame.cols/2)&&(X3.at<float>(0, 0)<frame.cols/2)))
+        {
+            putText(frame, "Turn left" ,Point(100,400) , FONT_HERSHEY_PLAIN, 2, Scalar(LaneColor4), 2, LINE_8);
+        }
+       
+    
+    //histogram equalization.
+    cvtColor(frame, hlsImg, COLOR_BGR2HLS_FULL);
+
+    split(hlsImg,channels);
+    equalizeHist(channels[1], channels[1]);
+    merge(channels, hlsImg);
+    cvtColor(hlsImg, frame, COLOR_HLS2BGR_FULL);
+    imshow("hlsImg", frame);
+    
+    //histogram graph.
+      }
+
+
+
+
+int main() {
+    char title[100] = "/Users/NAMSOO/Documents/Xcode/OpenCV/SelfDriving/SelfDriving/mono.mp4";
+    VideoCapture capture(title);
+    Mat frame;
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
     
     int key, frameNum = 1;
     int frame_rate = 30;
@@ -474,11 +758,18 @@ int main() {
     // videoRead
     while (capture.read(frame)) {
         
+<<<<<<< HEAD
         origin = frame.clone();
         lane_detection(frame);
         
         imshow("frame", frame); //show the original frame
         //imshow("origin",origin);
+=======
+        lane_detection(frame);
+        
+        imshow("frame", frame);
+        
+>>>>>>> 3f02d56a322a536c4bdc3b972aca4078ad86c24d
         key = waitKey(frame_rate);
         if (key == 32) {
             if (frame_rate == 30)
---
+++ MSAC_with Contour/VP_MSAC.cpp
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "MSAC.h"
+
+#define _cplusplus
+
+using namespace std;
+using namespace cv;
+
+void laneMarkingsDetector(Mat& srcGray, Mat& dstGray, int tau);
+vector<vector<Point> > findandDrawContour(Mat &roi, char* windowName);
+
+int main()
+{
+    Mat img;
+    VideoCapture cap;
+    char ch;
+    int fps = 10;
+    int fr_cnt = 0;
+    
+    cap.open("/Users/NAMSOO/Documents/Xcode/OpenCV/mono.mp4");
+    
+    while (1)
+    {
+        //cap >> img;
+        if (!cap.read(img))
+            break;
+        //imshow("Input Image", img);
+        Mat output;
+        output = img.clone();
+        
+        //convert type to gray
+        Mat gray;
+        cvtColor(img, gray, CV_BGR2GRAY);
+        
+        //get roi
+        Rect roiRect(0, gray.rows / 3 , gray.cols, gray.rows / 3 *2);
+        Mat roi = gray(roiRect).clone();
+        //imshow("ROI", roi);
+        Mat Roi;
+        copyMakeBorder(roi, Roi, gray.rows / 3, 0, 0, 0, BORDER_CONSTANT, Scalar(0, 0, 0));
+        //imshow("Roi_border", Roi);
+        
+        //apply marking detect algrithm
+        Mat MarkingResult(Roi.rows, Roi.cols, Roi.type());
+        int tau = 18;
+        laneMarkingsDetector(Roi, MarkingResult, tau);
+        imshow("Lane Marking Result", MarkingResult);
+        
+        //Thresholding
+        Mat thresh;
+        threshold(MarkingResult, thresh, 50, 255, THRESH_BINARY);
+        imshow("Threshold", thresh);
+        
+        
+        Mat cannyDetect;
+        Canny(thresh, cannyDetect, 150, 300, 3);
+        //Use Hough transform
+        Mat canny;
+        Canny(thresh, canny, 30, 100);
+        vector<vector<cv::Point>> lineSegments;
+        vector<cv::Point> aux;
+        vector<Vec4i> lines;
+        int houghThreshold = 70;
+        if (gray.cols*gray.rows < 400 * 400)
+            houghThreshold = 100;
+        
+        HoughLinesP(canny, lines, 1, CV_PI / 180, houghThreshold, 20, 10);
+        
+        while (lines.size() > 200)
+        {
+            lines.clear();
+            houghThreshold += 10;
+            cv::HoughLinesP(canny, lines, 1, CV_PI / 180, houghThreshold, 10, 10);
+        }
+        
+        for (size_t i = 0; i < lines.size(); i++)
+        {
+            Point pt1, pt2;
+            pt1.x = lines[i][0];
+            pt1.y = lines[i][1];
+            pt2.x = lines[i][2];
+            pt2.y = lines[i][3];
+            line(output, pt1, pt2, CV_RGB(0, 0, 0), 2);
+            
+            
+            // Store into vector of pairs of Points for msac
+            aux.clear();
+            aux.push_back(pt1);
+            aux.push_back(pt2);
+            lineSegments.push_back(aux);
+        }
+        
+        std::vector<cv::Mat> vps;
+        vector<int> numInliers;
+        std::vector<std::vector<std::vector<cv::Point> > > lineSegmentsClusters;
+        
+        // Initialization
+        MSAC msac;
+        msac.init(MODE_NIETO, Size(img.cols, img.rows), 1);
+        // Execution (passing as argument the lines obtained with the Hough transform)
+        msac.multipleVPEstimation(lineSegments, lineSegmentsClusters, numInliers, vps, 1);
+        
+        
+        if (!vps.empty()){
+            int x = (int)vps[0].at<float>(0, 0);
+            int y = (int)vps[0].at<float>(1, 0);
+            circle(output, Point(x, y), 5, Scalar(0, 0, 255), -1);
+        }
+        
+        char contourL3[10] = "ContourL3";
+        char contourR3[10] = "ContourR3";
+        char contourL4[10] = "ContourL4";
+        char contourR4[10] = "ContourR4";
+        
+        Mat grayl3, grayl4, grayr3, grayr4, frame_lane;
+        
+        int interest_y = 168; //ROI value of y
+        int interest_x = 0;
+        int width = img.cols - interest_x; //width of ROI
+        int height = img.rows - interest_y; //height of ROI
+        int halfWidth = width / 2;
+        int subROIHeight = height / 16;
+        
+        Point rec3_point(interest_x, interest_y + subROIHeight * 3);
+        Point rec3_Rpoint(interest_x + halfWidth, interest_y + subROIHeight * 3);
+        Rect rec3(rec3_point, Size(width, subROIHeight * 3)); // ROI3
+        
+        Point rec4_point(interest_x, interest_y + subROIHeight * 6);
+        Point rec4_Rpoint(interest_x + halfWidth, interest_y + subROIHeight * 6);
+        Rect rec4(rec4_point, Size(width, subROIHeight * 10));
+        
+        Rect left_rec3(rec3_point, Size(halfWidth, subROIHeight * 3));
+        Rect left_rec4(rec4_point, Size(halfWidth, subROIHeight * 10));
+        Rect right_rec3(rec3_Rpoint, Size(halfWidth, subROIHeight * 3));
+        Rect right_rec4(rec4_Rpoint, Size(halfWidth, subROIHeight * 10));
+        frame_lane = cannyDetect.clone();
+        
+        
+        grayl3 = frame_lane(left_rec3);
+        grayl4 = frame_lane(left_rec4);
+        grayr3 = frame_lane(right_rec3);
+        grayr4 = frame_lane(right_rec4);
+        
+        vector<vector<Point> > pointSetl3;
+        vector<vector<Point> > pointSetl4;
+        vector<vector<Point> > pointSetr3;
+        vector<vector<Point> > pointSetr4;
+        imshow("L11", grayl3);
+        pointSetl3 = findandDrawContour(grayl3, contourL3);
+        pointSetl4 = findandDrawContour(grayl4, contourL4);
+        pointSetr3 = findandDrawContour(grayr3, contourR3);
+        pointSetr4 = findandDrawContour(grayr4, contourR4);
+        
+        imshow("L1", grayl3);
+        //imshow("VPS", vps[0]);
+        imshow("Result", output);
+        
+        fr_cnt = (int)cap.get(CV_CAP_PROP_POS_FRAMES);
+        ch = waitKey(fps);
+        
+    }
+}
+
+void laneMarkingsDetector(Mat& srcGray, Mat& dstGray, int tau)
+{
+    dstGray.setTo(0);
+    
+    int aux = 0;
+    for (int j = 0; j < srcGray.rows; ++j)
+    {
+        unsigned char *ptRowSrc = srcGray.ptr<uchar>(j);
+        unsigned char *ptRowDst = dstGray.ptr<uchar>(j);
+        
+        for (int i = tau; i < srcGray.cols - tau; ++i)
+        {
+            if (ptRowSrc[i] != 0)
+            {
+                aux = 2 * ptRowSrc[i];
+                aux += -ptRowSrc[i - tau];
+                aux += -ptRowSrc[i + tau];
+                aux += -abs((int)(ptRowSrc[i - tau] - ptRowSrc[i + tau]));
+                
+                aux = (aux < 0) ? 0 : (aux);
+                aux = (aux > 255) ? 255 : (aux);
+                
+                ptRowDst[i] = (unsigned char)aux;
+                
+            }
+        }
+    }
+}
+
+vector<vector<Point> > findandDrawContour(Mat &roi, char* windowName)
+{
+    
+    vector<vector<Point> > contours;
+    int mode = RETR_EXTERNAL;
+    
+    dilate(roi, roi, Mat(), Point(-1, -1), 3);
+    erode(roi, roi, Mat(), Point(-1, -1), 3);
+    
+    int method = CHAIN_APPROX_NONE;
+    
+    vector<Vec4i> hierarchy;
+    
+    findContours(roi, contours, hierarchy, mode, method);
+    cout << "contours.size()=" << contours.size() << endl;
+    imshow(windowName, roi);
+    
+    cvtColor(roi, roi, COLOR_GRAY2BGR);
+    
+    if (contours.size() > 1 && contours.size() <= 20) {
+        Scalar color(255, 255, 10);
+        vector<Rect> rect(contours.size());
+        vector<Mat> matArr(contours.size());
+        
+        int k = 0, j = 0;
+        int sizeOfRect = 30;
+        
+        for (int i = 0; i < contours.size(); i++) {
+            Rect temp = boundingRect(Mat(contours[i]));
+            if (temp.area() > 30) {
+                rect[k] = temp;
+                k++;
+            }
+        }
+        
+        for (int i = 0; i < k; i++)
+            rectangle(roi, rect[i], color, 2, 8, 0);
+        
+        //imshow(windowName, roi);
+    }
+    
+    return contours;
+}
